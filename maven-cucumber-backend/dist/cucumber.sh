2856c6490f73a7165847741a61b52b32f6fc8e6c
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-03-02 12:20:04 +0100

    new build of action with dependencies

---
+++ maven-cucumber-backend/dist/cucumber.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+echo "Working directory:"
+pwd
+
+if [ -z "$INPUT_MAVEN_IMAGE" ]; then
+  >&2 echo "::error A name of an image cantaining java and maven must be provided, see https://hub.docker.com/_/maven"
+  exit 1;
+fi
+
+sudo rm -rf bidrag-cucumber-backend
+git clone --depth 1 https://github.com/navikt/bidrag-cucumber-backend
+cd bidrag-cucumber-backend
+
+if [ "$GITHUB_REF" != "refs/heads/master" ]; then
+  export ENVIRONMENT=q1
+else
+  export ENVIRONMENT=q0
+fi
+
+if [ -z "$MAVEN_USER_CREDENTIALS" ]; then
+  >&2 echo "::error No MAVEN_USER_CREDENTIALS for a nav user to set up security of the tests are provided, see bidrag-actions/maven-cucumber-bidrag/README.md"
+  exit 1;
+fi
+
+if [ -z "$MAVEN_TEST_USER_CREDENTIALS" ]; then
+  >&2 echo "::error No MAVEN_TEST_USER_CREDENTIALS for for the test suite are provided, see bidrag-actions/maven-cucumber-bidrag/README.md"
+  exit 1;
+fi
+
+if [ -z "$MAVEN_PIP_USER_CREDENTIALS" ]; then
+  docker run --rm -v $PWD:/usr/src/mymaven -v ~/.m2:/root/.m2 -w /usr/src/mymaven "$INPUT_MAVEN_IMAGE" mvn clean test \
+    -DENVIRONMENT="$ENVIRONMENT" \
+    "$MAVEN_USER_CREDENTIALS" \
+    "$MAVEN_TEST_USER_CREDENTIALS" \
+    -Dcucumber.options='--tags "@bidrag-dokument"'
+else
+  docker run --rm -v $PWD:/usr/src/mymaven -v ~/.m2:/root/.m2 -w /usr/src/mymaven "$INPUT_MAVEN_IMAGE" mvn clean test \
+    -DENVIRONMENT="$ENVIRONMENT" \
+    "$MAVEN_USER_CREDENTIALS" \
+    "$MAVEN_TEST_USER_CREDENTIALS" \
+    "$MAVEN_PIP_USER_CREDENTIALS" \
+    -Dcucumber.options='--tags "@bidrag-sak"'
+fi
