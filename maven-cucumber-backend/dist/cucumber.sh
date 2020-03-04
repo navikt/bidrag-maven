41f4b014b5eb53b455a7e0cf6a862222600105ca
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-03-04 11:51:54 +0000

    2020-03-04:11:51:54: Fresh build of actions with npm and ncc

--- maven-cucumber-backend/dist/cucumber.sh
+++ maven-cucumber-backend/dist/cucumber.sh
@@ -4,11 +4,6 @@ set -e
 echo "Working directory:"
 pwd
 
-if [ -z "$INPUT_MAVEN_IMAGE" ]; then
-  >&2 echo "::error A name of an image cantaining java and maven must be provided, see https://hub.docker.com/_/maven"
-  exit 1;
-fi
-
 cd "$RUNNER_WORKSPACE"
 sudo rm -rf bidrag-cucumber-backend
 git clone --depth 1 https://github.com/navikt/bidrag-cucumber-backend
@@ -20,18 +15,20 @@ else
   export ENVIRONMENT=q0
 fi
 
-if [ -z $USER_AUTHENTICATION ]; then
+if [ -z "$USER_AUTHENTICATION" ]; then
   >&2 echo "::error No USER_AUTHENTICATION (password) for a nav user are configured, see bidrag-actions/maven-cucumber-bidrag/README.md"
   exit 1;
 fi
 
-if [ -z $TEST_USER_AUTHENTICATION ]; then
+if [ -z "$TEST_USER_AUTHENTICATION" ]; then
   >&2 echo "::error No TEST_USER_AUTHENTICATION for for the test user are configured, see bidrag-actions/maven-cucumber-bidrag/README.md"
   exit 1;
 fi
 
-if [ -z $INPUT_PIP_USER ]; then
-  echo "Running in $ENVIRONMENT without PIP credentials"
+echo "Cucumber options: $INPUT_CUCUMBER_OPTIONS"
+
+if [ -z "$INPUT_PIP_USER" ]; then
+  echo "Envrironment: $ENVIRONMENT without PIP"
 
   docker run --rm -v $PWD:/usr/src/mymaven -v ~/.m2:/root/.m2 -w /usr/src/mymaven "$INPUT_MAVEN_IMAGE" mvn clean test \
     -DENVIRONMENT="$ENVIRONMENT" \
@@ -40,17 +37,17 @@ if [ -z $INPUT_PIP_USER ]; then
     -Dcucumber.options="$CUCUMBER_OPTIONS"
 
 else
-  echo "Running in $ENVIRONMENT with PIP credentials"
-
-  if [ -z $IPIP_USER_AUTHENTICATION ]; then
+  if [ -z "$IPIP_USER_AUTHENTICATION" ]; then
     >&2 echo "::error No PIP_USER_AUTHENTICATION for for the test user are configured, see bidrag-actions/maven-cucumber-bidrag/README.md"
     exit 1;
   fi
 
+  echo "Envrironment: $ENVIRONMENT with PIP"
+
   docker run --rm -v $PWD:/usr/src/mymaven -v ~/.m2:/root/.m2 -w /usr/src/mymaven "$INPUT_MAVEN_IMAGE" mvn clean test \
     -DENVIRONMENT="$ENVIRONMENT" \
-    "$MAVEN_USER_CREDENTIALS" \
-    "$MAVEN_TEST_USER_CREDENTIALS" \
-    "$MAVEN_PIP_USER_CREDENTIALS" \
-    -Dcucumber.options='--tags "@bidrag-sak"'
+    -DUSERNAME="$INPUT_USERNAME" -DUSER_AUTH="$USER_AUTHENTICATION" \
+    -DTEST_USER="$INPUT_TEST_USER" -DTEST_AUTH="$TEST_USER_AUTHENTICATION" \
+    -DPIP_USER="$INPUT_PIP_USER" -DPIP_AUTH="$PIP_USER_AUTHENTICATION" \
+    -Dcucumber.options="$CUCUMBER_OPTIONS"
 fi
