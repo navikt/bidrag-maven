47b6e03e1fe6b873c304b176210505739b7798bf
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-05-19 07:34:22 +0000

    2020-05-19:07:34:21: Build of actions with npm and ncc

--- cucumber-backend/dist/index.js
+++ cucumber-backend/dist/index.js
@@ -19,7 +19,13 @@ module.exports =
 /******/ 		};
 /******/
 /******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete installedModules[moduleId];
+/******/ 		}
 /******/
 /******/ 		// Flag the module as loaded
 /******/ 		module.l = true;
@@ -1502,7 +1508,7 @@ async function run() {
 
     // Execute cucumber bash script
     await exec.exec(
-        `${__dirname}/cucumber.sh ${doNotFail} ${mavenCommand} ${mavenImage} ${testUser} ${username}`
+        `${__dirname}/../cucumber.sh ${doNotFail} ${mavenCommand} ${mavenImage} ${testUser} ${username}`
     );
   } catch (error) {
     core.setFailed(error.message);
