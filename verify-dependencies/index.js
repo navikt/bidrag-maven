8ef75fe5b0bdd9174e2a668e9c2796bf6d29b38c
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-06-01 15:02:40 +0200

    use shell script from parent directory

--- verify-dependencies/index.js
+++ verify-dependencies/index.js
@@ -4,7 +4,7 @@ const exec = require("@actions/exec");
 async function run() {
   try {
     // Execute verify bash script
-    await exec.exec(`${__dirname}/verify.sh`);
+    await exec.exec(`${__dirname}/../verify.sh`);
   } catch (error) {
     core.setFailed(error.message);
   }
