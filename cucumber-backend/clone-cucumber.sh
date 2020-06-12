020cd9339502172d2dd20b4cda931d9594995f04
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-06-12 15:26:49 +0200

    bidrag-dokument-cucumber -> bidrag-cucumber-backend

--- cucumber-backend/clone-cucumber.sh
+++ cucumber-backend/clone-cucumber.sh
@@ -46,8 +46,8 @@ else
   git clone --depth 1 https://github.com/navikt/bidrag-cucumber-backend
 fi
 
-# gå til bidrag-dokument-cucumber slik at json filene blir synlige i docker container når ingegrasjonstestene gjøres
-cd bidrag-dokument-cucumber || exit 1;
+# gå til bidrag-cucumber-backend slik at json filene blir synlige i docker container når ingegrasjonstestene gjøres
+cd bidrag-cucumber-backend || exit 1;
 
 if [ "$INPUT_USE_NAIS_CONFIGURATION" == "true" ]; then
   CLONE_CUCUMBER_FOLDER=$PWD
