5b2a4b122e59c205b6a593694a10c56d36198429
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-08-03 11:02:38 +0200

    no cloning is done inside action

--- cucumber-backend/index.js
+++ cucumber-backend/index.js
@@ -5,27 +5,19 @@ async function run() {
   try {
     const useNaisConfiguration = core.getInput('use_nais_configuration');
     const runFromWorkspace = core.getInput('run_from_workspace');
+    const cucumberTag = core.getInput('cucumber_tag');
+    const doNotFail = core.getInput('do_not_fail');
+    const mavenCommand = core.getInput('maven_command');
+    const mavenImage = core.getInput('maven_image');
+    const testUser = core.getInput('test_user');
+    const username = core.getInput('username');
 
-    // Execute clone bash script to setup running bidrag-cucumber-backend
-    const exitCode = await exec.exec(
-        `${__dirname}/../clone-cucumber.sh`,
-        [useNaisConfiguration, runFromWorkspace]
+    // Execute cucumber bash script
+    await exec.exec(
+        `${__dirname}/../cucumber.sh`,
+        [cucumberTag, doNotFail, mavenCommand, mavenImage, runFromWorkspace,
+          testUser, useNaisConfiguration, username]
     );
-
-    if (exitCode === 0) {
-      const cucumberTag = core.getInput('cucumber_tag');
-      const doNotFail = core.getInput('do_not_fail');
-      const mavenCommand = core.getInput('maven_command');
-      const mavenImage = core.getInput('maven_image');
-      const testUser = core.getInput('test_user');
-      const username = core.getInput('username');
-
-      // Execute cucumber bash script
-      await exec.exec(
-          `${__dirname}/../cucumber.sh`,
-          [cucumberTag, doNotFail, mavenCommand, mavenImage, runFromWorkspace, testUser, useNaisConfiguration, username]
-      );
-    }
   } catch (error) {
     core.setFailed(error.message);
   }
