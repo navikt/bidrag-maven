2c78384311d4cd215445a40d3cba9320b4ddb309
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-08-04 16:02:28 +0000

    2020-08-04:16:02:28: Build of actions with npm and ncc

--- cucumber-backend/dist/index.js
+++ cucumber-backend/dist/index.js
@@ -1540,20 +1540,21 @@ const exec = __webpack_require__(960);
 
 async function run() {
   try {
-    const useNaisConfiguration = core.getInput('use_nais_configuration');
-    const runFromWorkspace = core.getInput('run_from_workspace');
     const cucumberTag = core.getInput('cucumber_tag');
     const doNotFail = core.getInput('do_not_fail');
     const mavenCommand = core.getInput('maven_command');
     const mavenImage = core.getInput('maven_image');
+    const runFromWorkspace = core.getInput('run_from_workspace');
     const testUser = core.getInput('test_user');
     const username = core.getInput('username');
 
     // Execute cucumber bash script
     await exec.exec(
         `${__dirname}/../cucumber.sh`,
-        [cucumberTag, doNotFail, mavenCommand, mavenImage, runFromWorkspace,
-          testUser, useNaisConfiguration, username]
+        [
+          cucumberTag, doNotFail, mavenCommand, mavenImage, runFromWorkspace,
+          testUser, username
+        ]
     );
   } catch (error) {
     core.setFailed(error.message);
