47df6b9d25f0b75df53501d19bc06a96a9458fa3
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-08-14 18:47:49 +0200

    dynamisk generering av settings.xml
    argument, repositories:
     - ex: repoIdEn=repoEn,repoIdTo=repoTo
     - default: maven-central=https://repo.maven.apache.org/maven2
    
    argument, maven_home:
     - ex: /path/til/maven/home
     - default: ~/.m2

--- setup/index.js
+++ setup/index.js
@@ -3,9 +3,11 @@ const exec = require("@actions/exec");
 
 async function run() {
   try {
+    const urls = core.getInput('repositories');
+    const m2home = core.getInput('maven_home');
 
     // Execute tag bash script
-    await exec.exec(`${__dirname}/../setup.sh`);
+    await exec.exec(`${__dirname}/../setup.sh`, [urls, m2home]);
 
   } catch (error) {
     core.setFailed(error.message);
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,37 +1,74 @@
 #!/bin/bash
-set -e
+set -x
 
-echo "Setup maven"
+######################################################################################
+#
+# Forutsetninger for script som lager en maven settings.xml
+#
+# - Maven repositories blir gitt som en kommaseparert liste med mavenrepo: id/navn=url
+# - Maven home som skal inneholde setting.xml blir gitt som argument til skript
+#
+# Følgende skjer i dette skriptet:
+# 1) setter inputs og leser kommaseparert liste inn i array
+# 2) for hvert innslag i den kommaseparerte lista
+#    - lag en <server>...</server> element
+#    - lag et <repository>...</repository> element
+# 3) legg til server og repository elementene i en settings.xml
+# 4) skriver den genererte settings.xml til k
+#
+######################################################################################
 
-MAVEN_REPO=~/.m2
+INPUT_REPOS=$1
+INPUT_MAVEN_HOME=$2
+
+if [ $# -ne 2 ]; then
+  echo "Usage: ./setup.sh <repo=https://repo.location>,<annet.repo=https://other.location>...,<...> <path_to_maven_home>"
+  exit 1
+fi
+
+if [ -z "$INPUT_MAVEN_HOME" ]; then
+  MAVEN_HOME=$HOME/.m2
+else
+  MAVEN_HOME=$INPUT_MAVEN_HOME
+fi
+
+IFS=','
+read -ra REPOS <<< "$INPUT_REPOS"
+
+IDS=()
+REPOS_XML=""
+SERVER_XML=""
+
+for repo in "${REPOS[@]}"; do
+  NAME=$(echo "$repo" | cut -d'=' -f 1)
+  IDS+=("$NAME")
+  URL=$(echo "$repo" | cut -d'=' -f 2)
+
+  REPOS_XML="${REPOS_XML}
+        <repository>
+          <id>$NAME</id>
+          <name>$NAME</name>
+          <url>$URL</url>
+        </repository>"
+done
+
+for id in "${IDS[@]}"; do
+  SERVER_XML="${SERVER_XML}
+    <server>
+      <id>$id</id>
+      <username>$GITHUB_ACTOR</username>
+      <password>$GITHUB_TOKEN</password>
+    </server>"
+done
+
 MAVEN_SETTINGS="
 <settings>
-  <servers>
-    <server>
-        <id>maven-release</id>
-        <username>$GITHUB_ACTOR</username>
-        <password>$GITHUB_TOKEN</password>
-    </server>
-    <server>
-        <id>github-package-registry-navikt</id>
-        <username>$GITHUB_ACTOR</username>
-        <password>$GITHUB_TOKEN</password>
-    </server>
+  <servers>$SERVER_XML
   </servers>
   <profiles>
     <profile>
       <id>default</id>
-      <repositories>
-        <repository>
-          <id>central</id>
-          <name>Maven central</name>
-          <url>https://repo.maven.apache.org/maven2</url>
-        </repository>
-        <repository>
-          <id>maven-release</id>
-          <name>GitHub felles Apache Maven Packages</name>
-          <url>https://maven.pkg.github.com/navikt/maven-release/</url>
-        </repository>
+      <repositories>$REPOS_XML
       </repositories>
     </profile>
   </profiles>
@@ -41,11 +78,11 @@ MAVEN_SETTINGS="
 </settings>
 "
 
-if [ -d "$MAVEN_REPO" ]; then
-  echo "Using existing $MAVEN_REPO"
+if [ -d "$MAVEN_HOME" ]; then
+  echo "Using existing $MAVEN_HOME"
 else
-  echo "Creating $MAVEN_REPO"
-  mkdir "$MAVEN_REPO"
+  echo "Creating $MAVEN_HOME"
+  mkdir "$MAVEN_HOME"
 fi
 
-echo "$MAVEN_SETTINGS" > "$MAVEN_REPO"/settings.xml
+echo "$MAVEN_SETTINGS" > "$MAVEN_HOME/settings.xml"
